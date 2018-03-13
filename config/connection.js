2d7a79885bdb5a2fbce05283f2dce5d171bdc82b
idaselma3025 <idaselma3025@gmail.com>
2018-03-13 08:29:49 -0500

    fixconnection

--- config/connection.js
+++ config/connection.js
@@ -23,5 +23,3 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
-
-mysql://beee20b8a00738:955e6edc@us-cdbr-iron-east-05.cleardb.net/heroku_cc8a4c6391e9dbf?reconnect=true
