ced961a874da5dd7adb12c4420e19ee34fc29fb9
idaselma3025 <idaselma3025@gmail.com>
2018-03-13 08:05:56 -0500

    tryagain

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,9 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
+	createdAt TIMESTAMP NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+Error Code: 1046. No database selected Select the default DB to be used by double-clicking its name in the SCHEMAS list in the sidebar.
+Error Code: 1046. No database selected Select the default DB to be used by double-clicking its name in the SCHEMAS list in the sidebar.
